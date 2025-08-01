c4abaad57cabf68eda5e2099d7565cf8f6a01c68
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-08-01 19:17:52 +0200

    add better safety comments

--- src/print/unix/dest.rs
+++ src/print/unix/dest.rs
@@ -15,8 +15,9 @@ impl CupsDestinations {
 	/// Creates a new instance of this struct, retrieving CUPS destinations.
 	pub fn new() -> Self {
 		let mut dests_ptr = ptr::null_mut();
-		// SAFETY: cupsGetDests accepts any pointer to a pointer, and will overwrite `dests_ptr` with
-		// the pointer to the allocated array.
+		// SAFETY: `cupsGetDests`` accepts a pointer to `*mut cups_dest_t`, allocates an array,
+		// populates the passed in pointer with a valid pointer to the array, and returns the number
+		// of elemenets. These are valid until `cupsFreeDests` is called.
 		let dests_num = unsafe { cups::cupsGetDests(&mut dests_ptr) };
 		Self {
 			dests: FatPointerMut {
@@ -27,12 +28,14 @@ impl CupsDestinations {
 	}
 	/// Returns an immutable view into the CUPS destinations as a slice.
 	pub fn as_slice(&self) -> &[cups::cups_dest_t] {
-		// SAFETY: `self.dests` contains a valid pointer & size from CUPS obtained in `Self::new`.
+		// SAFETY: `self.dests` is a valid fat pointer, pointing to memory allocated by CUPS.
+		// It remains valid until `cupsFreeDests` is called.
 		unsafe { fat_ptr_to_slice(&self.dests) }
 	}
 	/// Returns a mutable view into the CUPS destinations as a slice.
 	fn as_slice_mut(&mut self) -> &mut [cups::cups_dest_t] {
-		// SAFETY: `self.dests` contains a valid pointer & size from CUPS obtained in `Self::new`.
+		// SAFETY: `self.dests` is a valid fat pointer, pointing to memory allocated by CUPS.
+		// It remains valid until `cupsFreeDests` is called.
 		unsafe { fat_ptr_to_slice_mut(&self.dests) }
 	}
 	/// Returns a destination at the specified index, or [`None`] if the index is invalid.
@@ -43,7 +46,8 @@ impl CupsDestinations {
 
 impl Drop for CupsDestinations {
 	fn drop(&mut self) {
-		// SAFETY: `self.dests` contains a valid pointer & size from CUPS obtained in `Self::new`.
+		// SAFETY: `self.dests` is a valid fat pointer, pointing to memory allocated by CUPS.
+		// It remains valid until `cupsFreeDests` is called, which is now.
 		unsafe { cups::cupsFreeDests(self.dests.num, self.dests.ptr) };
 		// Seems like we don't have to drop the options on each destination ourselves (causes
 		// occasional double frees), and they're dropped by CUPS along with this call.
