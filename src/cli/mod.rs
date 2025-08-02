28884f5a8a7a8f08709c8988acea39b1d37dcd83
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-08-02 23:46:52 +0200

    restructure

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -3,19 +3,17 @@ use colored::Colorize;
 use crate::cli::args::{Args, Command};
 
 pub mod args;
+pub mod commands;
 mod common;
-mod display;
-mod list;
-mod print;
 mod snapshot;
 
 /// Runs the command specified in the [`Args`] instance.
 pub fn run_command(args: Args) {
 	match args.command {
-		Command::List => list::list(),
-		Command::Display(d_args) => display::display(d_args),
+		Command::List => commands::list(),
+		Command::Display(d_args) => commands::display(d_args),
 		Command::Print(p_args) => {
-			let result = print::print(p_args);
+			let result = commands::print(p_args);
 			if let Err(err) = result {
 				eprintln!("{} {}", "error:".bold().red(), err)
 			}
