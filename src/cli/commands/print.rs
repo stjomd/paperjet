5c2e25b09138b6f29df117eeee56ba1f84337e6d
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-08-05 22:17:34 +0200

    refactor

--- src/cli/commands/print.rs
+++ src/cli/commands/print.rs
@@ -56,63 +56,112 @@ fn map_path_to_file_result(path: &PathBuf) -> Result<File> {
 }
 
 fn duplex(mut files: Vec<File>, printer: Printer, options: PrintOptions) -> Result<()> {
+	// Validate amount of files
 	if files.len() != 1 {
 		bail!("exactly one file must be specified to print in duplex mode")
 	}
+	// Validate options
+	if options.copies.is_some() {
+		bail!(
+			"option '{}' is not supported in duplex mode",
+			"copies".yellow()
+		)
+	} else if options.number_up.is_some() {
+		bail!(
+			"option '{}' is not supported in duplex mode",
+			"number up".yellow()
+		)
+	} else if options.sides_mode.is_some() {
+		bail!(
+			"option '{}' is not supported in duplex mode",
+			"sides mode".yellow()
+		)
+	}
+
+	let pdfium = pdfium()?;
 	let file = files.remove(0);
-	let split = split_pdf(file)?;
 
-	println!("Printing one side...");
-	printrs::print([Cursor::new(split.0)], printer.clone(), options.clone())?;
-	println!("One side has been submitted.");
+	let (front_pdf, back_pdf) = split_pdf(&pdfium, file)?;
+	let (front_len, back_len) = (front_pdf.pages().len(), back_pdf.pages().len());
+	let (front_bytes, back_bytes) = pdfs_to_bytes(front_pdf, back_pdf)?;
 
-	print!("Once the printing has finished, turn the pages over and press Enter: ");
+	let sheets_num = u16::max(front_len, back_len);
+	println!(
+		"You will need {} {} of paper.",
+		sheets_num.to_string().bold().cyan(),
+		if sheets_num == 1 { "sheet" } else { "sheets" },
+	);
+
+	println!("\nPrinting the front side...");
+	printrs::print([Cursor::new(front_bytes)], printer.clone(), options.clone())?;
+	println!("The front side has been submitted.");
+
+	print!(
+		"\nOnce the printing has finished, turn the pages over and press {}: ",
+		"Enter".bold().cyan()
+	);
 	io::stdout().flush()?;
 	let mut input = String::new();
 	io::stdin().read_line(&mut input)?;
 
-	println!("Printing the other side...");
-	printrs::print([Cursor::new(split.1)], printer, options)?;
-	println!("The other side has been submitted.");
+	println!("\nPrinting the back side...");
+	printrs::print([Cursor::new(back_bytes)], printer, options)?;
+	println!("The back side has been submitted.");
 	Ok(())
 }
 
-fn split_pdf(pdf_file: File) -> Result<SplitPdf> {
-	let pdfium = Pdfium::new(
+/// Loads PDFium as a dynamic library.
+fn pdfium() -> Result<Pdfium> {
+	Ok(Pdfium::new(
 		Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path("./"))
 			.or_else(|_| Pdfium::bind_to_system_library())
-			.map_err(|e| match e {
-				PdfiumError::LoadLibraryError(_e) => anyhow!("could not link PDFium: {_e}"),
-				_e => anyhow!("could not link PDFium: {_e}"),
+			.map_err(|_e| match _e {
+				PdfiumError::LoadLibraryError(e) => anyhow!("could not link PDFium: {e}"),
+				e => anyhow!("could not link PDFium: {e}"),
 			})?,
-	);
-
-	let source = pdfium.load_pdf_from_reader(pdf_file, None)?;
-	let mut even = pdfium.create_new_pdf()?;
-	let mut odd = pdfium.create_new_pdf()?;
-
-	for i in 0..source.pages().len() {
-		if i % 2 == 0 {
-			let j = even.pages().len();
-			even.pages_mut().copy_page_from_document(&source, i, j)?;
-		} else {
-			odd.pages_mut().copy_page_from_document(&source, i, 0)?;
-		};
-	}
-	if odd.pages().len() < even.pages().len() {
-		let page_size = source.pages().page_size(0)?;
-		odd.pages_mut()
-			.create_page_at_start(PdfPagePaperSize::Custom(
-				page_size.width(),
-				page_size.height(),
-			))?;
-	}
-
-	let split = SplitPdf(
-		even.save_to_bytes().map_err(anyhow::Error::from)?,
-		odd.save_to_bytes().map_err(anyhow::Error::from)?,
-	);
-	Ok(split)
+	))
 }
 
-struct SplitPdf(Vec<u8>, Vec<u8>);
+/// Splits a provided `pdf_file` into two PDF documents: for the front and back side.
+fn split_pdf<'a>(pdfium: &'a Pdfium, pdf_file: File) -> Result<(PdfDocument<'a>, PdfDocument<'a>)> {
+	let source = pdfium.load_pdf_from_reader(pdf_file, None)?;
+	let mut front = pdfium.create_new_pdf()?;
+	let mut back = pdfium.create_new_pdf()?;
+
+	// Alternate between copying pages to `front` and `back`.
+	// When printing the back side, the printed front sides will be flipped, thus we need to copy
+	// the back side pages in reverse order.
+	for i in 0..source.pages().len() {
+		if i % 2 == 0 {
+			let j = front.pages().len();
+			front.pages_mut().copy_page_from_document(&source, i, j)?;
+		} else {
+			back.pages_mut().copy_page_from_document(&source, i, 0)?;
+		};
+	}
+
+	// If the total amount of pages is uneven, we need to insert one blank page at the beginning
+	// of the back side - this aligns the two prints.
+	let page_size = source.pages().page_size(0)?;
+	align_sides(&mut front, &mut back, page_size)?;
+
+	Ok((front, back))
+}
+
+/// Aligns two split PDF documents, `front` and `back`, by inserting a single blank page
+/// at the beginning of the back side document.
+fn align_sides(front: &mut PdfDocument, back: &mut PdfDocument, dimensions: PdfRect) -> Result<()> {
+	if back.pages().len() != front.pages().len() {
+		back.pages_mut()
+			.create_page_at_start(PdfPagePaperSize::Custom(
+				dimensions.width(),
+				dimensions.height(),
+			))?;
+	}
+	Ok(())
+}
+
+/// Converts two split PDF documents, `even` and `odd`, to bytes.
+fn pdfs_to_bytes<'a>(even: PdfDocument<'a>, odd: PdfDocument<'a>) -> Result<(Vec<u8>, Vec<u8>)> {
+	Ok((even.save_to_bytes()?, odd.save_to_bytes()?))
+}
