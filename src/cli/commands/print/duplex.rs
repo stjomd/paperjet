5f9f7a9b62cec9de2bc60846275809c9b4c2f66d
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-08-05 23:36:51 +0200

    add unit tests

--- src/cli/commands/print/duplex.rs
+++ src/cli/commands/print/duplex.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{self, Cursor, Write};
+use std::io::{self, Cursor, Read, Seek, Write};
 
 use anyhow::{Result, anyhow, bail};
 use colored::Colorize;
@@ -75,8 +75,11 @@ fn pdfium() -> Result<Pdfium> {
 }
 
 /// Splits a provided `pdf_file` into two PDF documents: for the front and back side.
-fn split_pdf<'a>(pdfium: &'a Pdfium, pdf_file: File) -> Result<(PdfDocument<'a>, PdfDocument<'a>)> {
-	let source = pdfium.load_pdf_from_reader(pdf_file, None)?;
+fn split_pdf<'a, R>(pdfium: &'a Pdfium, reader: R) -> Result<(PdfDocument<'a>, PdfDocument<'a>)>
+where
+	R: Read + Seek,
+{
+	let source = pdfium.load_pdf_from_reader(reader, None)?;
 	let pages_len = source.pages().len();
 	if pages_len < 2 {
 		bail!(
@@ -125,4 +128,218 @@ fn align_sides(front: &mut PdfDocument, back: &mut PdfDocument, dimensions: PdfR
 /// Converts two split PDF documents, `even` and `odd`, to bytes.
 fn pdfs_to_bytes<'a>(even: PdfDocument<'a>, odd: PdfDocument<'a>) -> Result<(Vec<u8>, Vec<u8>)> {
 	Ok((even.save_to_bytes()?, odd.save_to_bytes()?))
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn if_empty_pdf_then_split_returns_err() {
+		let pdfium = pdfium().expect("PDFium should be available, but isn't");
+
+		// Create an empty PDF file:
+		let pdf = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium")
+			.save_to_bytes()
+			.expect("Could not save PDF to bytes");
+		let reader = Cursor::new(pdf);
+
+		// Attempt to split the file: it has 0 pages, and thus fails
+		let result = split_pdf(&pdfium, reader);
+		assert!(
+			result.is_err(),
+			"split_pdf should return an error, but didn't"
+		);
+	}
+
+	#[test]
+	fn if_pdf_with_one_page_then_split_returns_err() {
+		let pdfium = pdfium().expect("PDFium should be available, but isn't");
+
+		// Create a PDF file with one page:
+		let mut pdf = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		pdf.pages_mut()
+			.create_page_at_end(PdfPagePaperSize::a4())
+			.expect("Could not create a PDF page");
+		let pdf = pdf.save_to_bytes().expect("Could not save PDF to bytes");
+		let reader = Cursor::new(pdf);
+
+		// Attempt to split the file: it has 1 page, and thus fails
+		let result = split_pdf(&pdfium, reader);
+		assert!(
+			result.is_err(),
+			"split_pdf should return an error, but didn't"
+		);
+	}
+
+	#[test]
+	fn if_pdf_with_two_pages_then_split_returns_ok() {
+		let pdfium = pdfium().expect("PDFium should be available, but isn't");
+
+		// Create a PDF file with two pages:
+		let mut pdf = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		pdf.pages_mut()
+			.create_page_at_end(PdfPagePaperSize::a4())
+			.expect("Could not create a PDF page");
+		pdf.pages_mut()
+			.create_page_at_end(PdfPagePaperSize::a4())
+			.expect("Could not create a PDF page");
+		let pdf = pdf.save_to_bytes().expect("Could not save PDF to bytes");
+		let reader = Cursor::new(pdf);
+
+		// Attempt to split the file: it has 2 page, and thus returns Ok
+		let result = split_pdf(&pdfium, reader);
+		assert!(
+			result.is_ok(),
+			"split_pdf should return Ok, but returned: {result:?}",
+		);
+	}
+
+	#[test]
+	fn if_pdf_with_even_amount_of_pages_then_split_returns_documents_with_same_length() {
+		let pdfium = pdfium().expect("PDFium should be available, but isn't");
+
+		// Create a PDF file with four pages:
+		let mut pdf = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		for _ in 0..4 {
+			pdf.pages_mut()
+				.create_page_at_end(PdfPagePaperSize::a4())
+				.expect("Could not create a PDF page");
+		}
+		let pdf = pdf.save_to_bytes().expect("Could not save PDF to bytes");
+		let reader = Cursor::new(pdf);
+
+		// Split the file:
+		let (front, back) = split_pdf(&pdfium, reader).expect("Could not split PDF");
+
+		// Front and back should have the same number of pages:
+		assert_eq!(
+			front.pages().len(),
+			back.pages().len(),
+			"front and back must have the same amount of pages"
+		);
+	}
+
+	#[test]
+	fn if_pdf_with_uneven_amount_of_pages_then_split_returns_documents_with_same_length() {
+		let pdfium = pdfium().expect("PDFium should be available, but isn't");
+
+		// Create a PDF file with five pages:
+		let mut pdf = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		for _ in 0..5 {
+			pdf.pages_mut()
+				.create_page_at_end(PdfPagePaperSize::a4())
+				.expect("Could not create a PDF page");
+		}
+		let pdf = pdf.save_to_bytes().expect("Could not save PDF to bytes");
+		let reader = Cursor::new(pdf);
+
+		// Split the file:
+		let (front, back) = split_pdf(&pdfium, reader).expect("Could not split PDF");
+
+		// Front and back should have the same amount of pages (be aligned):
+		assert_eq!(
+			front.pages().len(),
+			back.pages().len(),
+			"front and back must have the same amount of pages"
+		);
+	}
+
+	#[test]
+	fn if_diff_amount_of_pages_then_align_should_even_out_amount_of_pages() {
+		let pdfium = pdfium().expect("PDFium should be available, but isn't");
+
+		// Mock front and back with uneven amount of pages (back has 1 less):
+		let pages_len = 3;
+		let mut front = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		for _ in 0..pages_len {
+			front
+				.pages_mut()
+				.create_page_at_end(PdfPagePaperSize::a4())
+				.expect("Could not create a PDF page");
+		}
+		let mut back = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		for _ in 0..(pages_len - 1) {
+			back.pages_mut()
+				.create_page_at_end(PdfPagePaperSize::a4())
+				.expect("Could not create a PDF page");
+		}
+
+		// Align should even the amounts out:
+		let dimensions = PdfPagePaperSize::a4().as_rect();
+		align_sides(&mut front, &mut back, dimensions).expect("Could not align front and back");
+		assert_eq!(
+			front.pages().len(),
+			back.pages().len(),
+			"front and back must have the same number of pages after aligning"
+		);
+		assert_eq!(
+			front.pages().len(),
+			pages_len,
+			"front pages amount must remain unchanged"
+		);
+		assert_eq!(
+			back.pages().len(),
+			pages_len,
+			"back pages amount must increase by one"
+		);
+	}
+
+	#[test]
+	fn if_same_amount_of_pages_then_align_should_not_change_amount_of_pages() {
+		let pdfium = pdfium().expect("PDFium should be available, but isn't");
+
+		// Mock front and back with same amount of pages:
+		let pages_len = 2;
+		let mut front = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		for _ in 0..pages_len {
+			front
+				.pages_mut()
+				.create_page_at_end(PdfPagePaperSize::a4())
+				.expect("Could not create a PDF page");
+		}
+		let mut back = pdfium
+			.create_new_pdf()
+			.expect("Could not create new PDF with PDFium");
+		for _ in 0..pages_len {
+			back.pages_mut()
+				.create_page_at_end(PdfPagePaperSize::a4())
+				.expect("Could not create a PDF page");
+		}
+
+		// Align should do nothing:
+		let dimensions = PdfPagePaperSize::a4().as_rect();
+		align_sides(&mut front, &mut back, dimensions).expect("Could not align front and back");
+		assert_eq!(
+			front.pages().len(),
+			back.pages().len(),
+			"front and back must have the same number of pages after aligning"
+		);
+		assert_eq!(
+			front.pages().len(),
+			pages_len,
+			"front pages amount must remain unchanged"
+		);
+		assert_eq!(
+			back.pages().len(),
+			pages_len,
+			"back pages amount must remain unchanged"
+		)
+	}
 }
