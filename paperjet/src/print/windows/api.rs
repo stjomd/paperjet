cef7885c3f65ad3fc8e68ef35ba10b89a7768a22
Artem Zhukov <64159790+stjomd@users.noreply.github.com>
2025-08-16 09:03:17 +0200

    implement default printer

--- paperjet/src/print/windows/api.rs
+++ paperjet/src/print/windows/api.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use std::io::Read;
-use std::slice;
+use std::{ptr, slice};
 
-use windows::Win32::Graphics::Printing::{self, EnumPrintersW};
+use windows::Win32::Graphics::Printing;
+use windows::core::PWSTR;
 
 use crate::error::PrintError;
 use crate::options::PrintOptions;
@@ -16,7 +17,7 @@ impl CrossPlatformApi for PlatformSpecificApi {
 		// First call to determine the buffer size (we use level 4 here, thus this is fast)
 		// SAFETY: the only pointers we pass in are `buf_size` and `printers_len`, which are valid.
 		let _ = unsafe {
-			EnumPrintersW(
+			Printing::EnumPrintersW(
 				Printing::PRINTER_ENUM_LOCAL | Printing::PRINTER_ENUM_CONNECTIONS,
 				None,
 				4,
@@ -30,10 +31,10 @@ impl CrossPlatformApi for PlatformSpecificApi {
 		// by the previous call to `EnumPrintersW`.
 		let mut buf = vec![0u8; buf_size as usize];
 		let result = unsafe {
-			EnumPrintersW(
+			Printing::EnumPrintersW(
 				Printing::PRINTER_ENUM_LOCAL | Printing::PRINTER_ENUM_CONNECTIONS,
 				None,
-				5,
+				4,
 				Some(&mut buf),
 				&mut buf_size,
 				&mut printers_len,
@@ -51,7 +52,7 @@ impl CrossPlatformApi for PlatformSpecificApi {
 				printers_len as usize,
 			)
 			.iter()
-			.map(map_printer_info_4_to_printer)
+			.map(|info4| map_printer_info_4_to_printer(info4))
 			.collect::<Result<Vec<_>, _>>()
 		}
 	}
@@ -61,7 +62,29 @@ impl CrossPlatformApi for PlatformSpecificApi {
 	}
 
 	fn get_default_printer() -> Option<Printer> {
-		todo!("Not supported on Windows yet")
+		let mut len = 0;
+
+		// First call to determine the string length
+		// SAFETY: the first argument can be a null pointer, and the second is a valid pointer to an
+		// integer.
+		let _ = unsafe { Printing::GetDefaultPrinterW(PWSTR(ptr::null_mut()), &mut len) };
+
+		// Second call to fill the buffer with the string bytes
+		// SAFETY: both pointers are valid and will be populated with valid contents by Windows.
+		let mut buf = vec![0u16; len as usize];
+		let status = unsafe { Printing::GetDefaultPrinterW(PWSTR(buf.as_mut_ptr()), &mut len) };
+		if !status.as_bool() {
+			return None;
+		}
+
+		let name = String::from_utf16_lossy(&buf[..(buf.len() - 1)]);
+		Some(Printer {
+			identifier: name.clone(),
+			name,
+			instance: None,
+			is_default: true,
+			options: HashMap::new(),
+		})
 	}
 
 	fn print<I, R>(_readers: I, _printer: Printer, _options: PrintOptions) -> Result<(), PrintError>
@@ -74,10 +97,24 @@ impl CrossPlatformApi for PlatformSpecificApi {
 }
 
 /// Converts the [`PRINTER_INFO_4W`] instance to a [`Printer`] instance.
-fn map_printer_info_4_to_printer(info: &Printing::PRINTER_INFO_4W) -> Result<Printer, PrintError> {
+///
+/// # Safety
+/// `info` must be a valid reference to a valid `PRINTER_INFO_4W`.
+unsafe fn map_printer_info_4_to_printer(
+	info: &Printing::PRINTER_INFO_4W,
+) -> Result<Printer, PrintError> {
+	// SAFETY: info is a valid `PRINTER_INFO_4W` instance, as per the precondition.
+	let identifier = unsafe { info.pPrinterName.to_string()? };
+
+	if let Some(default_printer) = crate::get_default_printer() {
+		if default_printer.identifier == identifier {
+			return Ok(default_printer);
+		}
+	};
+
 	Ok(Printer {
-		identifier: unsafe { info.pPrinterName.to_string()? },
-		name: unsafe { info.pPrinterName.to_string()? },
+		identifier: identifier.clone(),
+		name: identifier,
 		instance: None,
 		is_default: false,
 		options: HashMap::new(),
